148c7fde8bbc21b1e0271d6e91f47e403e4affdd
Your Name <akarpun94@gmail.com>
2017-06-13 23:32:29 +0300

    initial commit

---
+++ app/src/main/java/com/twinkle94/monthyearpickerdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.twinkle94.monthyearpickerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
